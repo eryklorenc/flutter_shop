407e803e28c21f5c74ecc475dcf993ec341a3d00
Eryk Lorenc <reus2000@wp.pl>
2023-05-12 18:12:39 +0200

    Add get it and injectable

---
+++ lib/app/core/injection_container.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_shop/app/core/injection_container.config.dart';
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+
+final getIt = GetIt.instance;
+
+@InjectableInit()
+void configureDependencies() => getIt.init();
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_shop/app/core/injection_container.dart';
 import 'package:flutter_shop/app/welcome_page.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+   configureDependencies();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
