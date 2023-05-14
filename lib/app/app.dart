8f01d5668a7bb91704e5f7a71a7ccdcbcce7c60c
Eryk Lorenc <reus2000@wp.pl>
2023-05-14 17:24:17 +0200

    Organize files

---
+++ lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_shop/app/welcome_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+        primary: Colors.black,
+      )),
+      home: const WelcomePage(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_shop/app/app.dart';
 import 'package:flutter_shop/app/core/injection_container.dart';
-import 'package:flutter_shop/app/welcome_page.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -24,17 +24,4 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          colorScheme: ColorScheme.fromSwatch().copyWith(
-        primary: Colors.black,
-      )),
-      home: const WelcomePage(),
-    );
-  }
-}
+
