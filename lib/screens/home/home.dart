656834551e2b0d7b028e4b51bb8e7e2cddfd90ae
Eryk Lorenc <reus2000@wp.pl>
2023-06-12 19:23:59 +0200

    Add recommendations category

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -31,71 +31,101 @@ class _HomePageState extends State<HomePage> {
           color: Colors.black,
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(25),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(25),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Text(
-                  'Choose \na category',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.black,
-                  ),
-                ),
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    TextButton(
-                      onPressed: () {
-                        setState(() {
-                          menButtonSelected = true;
-                          womenButtonSelected = false;
-                        });
-                      },
-                      style: menButtonSelected
-                          ? ButtonStyle(
-                              foregroundColor:
-                                  MaterialStateProperty.all<Color>(Colors.grey),
-                            )
-                          : null,
-                      child: const Text(
-                        'Men',
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
+                    const Text(
+                      'Choose \na category',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.black,
                       ),
                     ),
-                    const SizedBox(
-                      width: 20.0,
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        setState(() {
-                          womenButtonSelected = true;
-                          menButtonSelected = false;
-                        });
-                      },
-                      style: womenButtonSelected
-                          ? ButtonStyle(
-                              foregroundColor:
-                                  MaterialStateProperty.all<Color>(Colors.grey),
-                            )
-                          : null,
-                      child: const Text(
-                        'Women',
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
-                      ),
+                    Row(
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              menButtonSelected = true;
+                              womenButtonSelected = false;
+                            });
+                          },
+                          style: menButtonSelected
+                              ? ButtonStyle(
+                                  foregroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Colors.orange),
+                                )
+                              : null,
+                          child: const Text(
+                            'Men',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 20.0,
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              womenButtonSelected = true;
+                              menButtonSelected = false;
+                            });
+                          },
+                          style: womenButtonSelected
+                              ? ButtonStyle(
+                                  foregroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Colors.orange),
+                                )
+                              : null,
+                          child: const Text(
+                            'Women',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
               ],
             ),
-          ],
-        ),
+          ),
+          Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(16),
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    'Recommended for you',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                physics: BouncingScrollPhysics(),
+                padding: EdgeInsets.symmetric(
+                  horizontal: 20,
+                ),
+                child: Row(
+                  children: [],
+                ),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
