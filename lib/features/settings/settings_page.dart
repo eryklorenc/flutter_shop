116042d31fc05b016172db721cc6fa0341a89e2d
eryklorenc <reus2000@wp.pl>
2023-09-05 14:50:04 +0200

    Add view to settings

--- lib/features/settings/settings_page.dart
+++ lib/features/settings/settings_page.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shop/app/core/theme/app_colors.dart';
+import 'package:flutter_shop/features/settings/widgets/setting_option.dart';
 import 'package:flutter_shop/generated/l10n.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({
     super.key,
   });
+
+  static const EdgeInsets customEdgeInsetsTop = EdgeInsets.only(top: 20);
+  static const Divider customDivider = Divider(
+    thickness: 2,
+    color: AppColors.dark,
+    indent: 25,
+    endIndent: 30,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -17,9 +26,7 @@ class SettingsPage extends StatelessWidget {
         centerTitle: true,
         title: Text(
           S.of(context).settings,
-          style: const TextStyle(
-            color: AppColors.dark,
-          ),
+          style: Theme.of(context).textTheme.titleLarge,
         ),
         leading: Padding(
           padding: const EdgeInsets.only(left: 8),
@@ -35,10 +42,60 @@ class SettingsPage extends StatelessWidget {
           ),
         ),
       ),
-      body: const Center(
-        child: Text(
-          'Settings',
-        ),
+      body: Column(
+        children: [
+          Padding(
+            padding: customEdgeInsetsTop,
+            child: SettingOption(
+              icon: const Icon(
+                Icons.edit_outlined,
+                color: AppColors.dark,
+                size: 28,
+              ),
+              title: 'Edytuj dane osobiste',
+              onTap: () {},
+            ),
+          ),
+          customDivider,
+          Padding(
+            padding: customEdgeInsetsTop,
+            child: SettingOption(
+              icon: const Icon(
+                Icons.lock_outline,
+                color: AppColors.dark,
+                size: 28,
+              ),
+              title: 'Prywatność i bezpieczeństwo',
+              onTap: () {},
+            ),
+          ),
+          customDivider,
+          Padding(
+            padding: customEdgeInsetsTop,
+            child: SettingOption(
+              icon: const Icon(
+                Icons.lock_clock_outlined,
+                color: AppColors.dark,
+                size: 28,
+              ),
+              title: 'Polityka prywatności',
+              onTap: () {},
+            ),
+          ),
+          customDivider,
+          Padding(
+            padding: customEdgeInsetsTop,
+            child: SettingOption(
+              icon: const Icon(
+                Icons.book,
+                color: AppColors.dark,
+                size: 28,
+              ),
+              title: 'Regulamin',
+              onTap: () {},
+            ),
+          ),
+        ],
       ),
     );
   }
