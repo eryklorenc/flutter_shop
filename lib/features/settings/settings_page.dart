985e953276aee13568a1afc2b5bd8b34c163326a
eryklorenc <reus2000@wp.pl>
2023-09-05 14:06:04 +0200

    Add AppBar

--- lib/features/settings/settings_page.dart
+++ lib/features/settings/settings_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shop/app/core/theme/app_colors.dart';
+import 'package:flutter_shop/generated/l10n.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({
@@ -7,8 +9,33 @@ class SettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      backgroundColor: AppColors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          S.of(context).settings,
+          style: const TextStyle(
+            color: AppColors.dark,
+          ),
+        ),
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 8),
+          child: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(
+              Icons.arrow_back,
+              size: 30,
+            ),
+            color: AppColors.dark,
+          ),
+        ),
+      ),
+      body: const Center(
         child: Text(
           'Settings',
         ),
