c50152b7d34ec42f39dfb6734daf85ffb48714a4
eryklorenc <reus2000@wp.pl>
2023-09-20 12:33:36 +0200

    add logic to edit profile

--- lib/features/settings/settings_page.dart
+++ lib/features/settings/settings_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_shop/app/core/common/custom_app_bar.dart';
 import 'package:flutter_shop/app/core/theme/app_colors.dart';
+import 'package:flutter_shop/features/profile/profile_page.dart';
 import 'package:flutter_shop/features/settings/widgets/privacy_and_security_page.dart';
 import 'package:flutter_shop/features/settings/widgets/setting_option.dart';
 import 'package:flutter_shop/generated/l10n.dart';
@@ -36,7 +37,13 @@ class SettingsPage extends StatelessWidget {
                 size: 28,
               ),
               title: S.of(context).edit_profile,
-              onTap: () {},
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => const ProfilePage(),
+                  ),
+                );
+              },
             ),
           ),
           customDivider,
